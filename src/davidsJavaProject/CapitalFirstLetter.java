7af81f2ca8a09539feb7f1ffe39fa30831afefe7
Davidtank70 <Davidtank70@gmail.com>
2022-06-02 12:53:15 -0400

    This is a test push and commit

--- src/davidsJavaProject/CapitalFirstLetter.java
+++ src/davidsJavaProject/CapitalFirstLetter.java
@@ -3,5 +3,6 @@ package davidsJavaProject;
 public class CapitalFirstLetter {
 	public static void main (String[] args) {
 		System.out.println("Hello World");
+		System.out.println("Hope this Works");
 	}
 }
